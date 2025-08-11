4c0899123b84f615daa19bab1997e55b2169e313
EC2 Default User <ec2-user@ip-172-31-46-164.ap-southeast-2.compute.internal>
2025-08-11 07:32:06 +0000

    [Finish] 4-9

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  
+  resources :post_images, only: [:new, :index, :show]
+
   get 'homes/about' , to: 'homes#about' , as: 'about'
   devise_scope :user do #AI質問にて追加
     get '/users/sign_out' => 'devise/sessions#destroy'
